fc57f4c9fbbfe4aa5d4311bdf0213bcd15e6c68b
ragulkannan99 <ragulkannanshanthiganesan@gmail.com>
2025-02-11 23:34:29 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Varsha Medical Chatbot',
+    version= '0.0.0',
+    author= 'Ragulkannan99',
+    author_email= 'ragulkannanshanthiganesan@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
